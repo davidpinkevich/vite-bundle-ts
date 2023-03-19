e9f861cfb406b09a8a9c94a9a9832913aee69ff4
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-19 18:38:44 +0300

    feat: add coverage

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,9 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/tests/setupTests.ts'],
+    coverage: {
+      provider: 'c8',
+      all: true,
+    },
   },
 });
